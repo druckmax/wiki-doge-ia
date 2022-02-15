b0a59460d8da84cc143f99272cbbc9351fcdc056
druckmax <max_sommerfeld@web.de>
2022-02-15 23:47:41 +0100

    adding hamburger menu incl. javascript

---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+// THIS IS THE LONG VERSION
+
+// let toggle = document.getElementById("aside");
+
+// let noShow = true;
+
+// function showAside () {
+//     if (noShow) {
+//         toggle.style.display = "block";
+//         noShow = false;
+//     } else {
+//         toggle.style.display = "none";
+//         noShow = true;
+//     }
+// }
+
+// Defining the element "toggle" by getting the aside element from the DOM
+
+let toggle = document.getElementById("aside");
+
+let noShow = true;
+
+// Naming the function "showAside" and toggling the aside menu by adding and removing the ".show" class defined in the CSS to the aside element in the HTML
+
+function showAside() {
+    noShow =! noShow;
+    toggle.classList.toggle("show", noShow);
+}
